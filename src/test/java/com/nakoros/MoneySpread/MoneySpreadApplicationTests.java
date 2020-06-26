1ba3191bfb5e9e50571b66546d069401e1583b7d
ydh21 <ydh21@DESKTOP-UPPGFE0>
2020-06-26 14:02:40 +0900

    first

---
+++ src/test/java/com/nakoros/MoneySpread/MoneySpreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakoros.MoneySpread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneySpreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
